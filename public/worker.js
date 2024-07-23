de89fb16fbeb415edbe4e075003363a1b6d56e11
eldinor <eldinor@gmail.com>
2024-07-23 03:25:53 +0300

    worker test

---
+++ public/worker.js
@@ -0,0 +1,9 @@
+importScripts("@gl");
+onmessage = (e) => {
+  console.log("Message received from main script", e.data);
+ // const workerResult = e.data * 3;
+
+ const workerResult =  new Blob([e.data]);
+  console.log("Posting message back to main script");
+  postMessage(workerResult);
+};
