f4e3011bfe8abf9075c4156021bd080f6d72b08e
eldinor <eldinor@gmail.com>
2024-08-09 17:08:51 +0300

    Some worker test

--- src/playground/main-scene.ts
+++ src/playground/main-scene.ts
@@ -1,27 +1,45 @@
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
 // import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-import '@babylonjs/loaders'
+import "@babylonjs/loaders";
 
-import { Ground } from './ground'
+import { Ground } from "./ground";
+
+import WorkerURL from "workerpool";
+import workerpool from "workerpool";
 
 export default class MainScene {
-  private camera: BABYLON.ArcRotateCamera
+  private camera: BABYLON.ArcRotateCamera;
 
-  constructor(private scene: BABYLON.Scene, private canvas: HTMLCanvasElement, private engine: BABYLON.Engine) {
-    this._setCamera(scene)
-    this._setLight(scene)
-    this.loadComponents()
+  constructor(
+    private scene: BABYLON.Scene,
+    private canvas: HTMLCanvasElement,
+    private engine: BABYLON.Engine
+  ) {
+    this._setCamera(scene);
+    this._setLight(scene);
+    this.loadComponents();
   }
 
   _setCamera(scene: BABYLON.Scene): void {
-    this.camera = new BABYLON.ArcRotateCamera('camera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(80), 20, BABYLON.Vector3.Zero(), scene)
-    this.camera.attachControl(this.canvas, true)
-    this.camera.setTarget(BABYLON.Vector3.Zero())
+    this.camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      BABYLON.Tools.ToRadians(90),
+      BABYLON.Tools.ToRadians(80),
+      20,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+    this.camera.attachControl(this.canvas, true);
+    this.camera.setTarget(BABYLON.Vector3.Zero());
   }
 
   _setLight(scene: BABYLON.Scene): void {
-    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene)
-    light.intensity = 0.7
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+    light.intensity = 0.7;
   }
 
   // _setPipeLine(): void {
@@ -30,6 +48,14 @@ export default class MainScene {
 
   loadComponents(): void {
     // Load your files in order
-    new Ground(this.scene)
+    //  new Ground(this.scene)
+
+    const pool = workerpool.pool(WorkerURL, {
+      maxWorkers: 3,
+      workerOpts: {
+        // By default, Vite uses a module worker in dev mode, which can cause your application to fail. Therefore, we need to use a module worker in dev mode and a classic worker in prod mode.
+        type: import.meta.env.PROD ? undefined : "module",
+      },
+    });
   }
 }
