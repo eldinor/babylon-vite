8ab472351581e819decbebd5279f6a9dfb412a5c
eldinor <eldinor@gmail.com>
2024-10-15 16:37:32 +0300

    some changes

--- src/playground/ground.ts
+++ src/playground/ground.ts
@@ -1,23 +1,31 @@
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-import '@babylonjs/loaders'
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+import "@babylonjs/loaders";
 
 export class Ground {
   constructor(private scene: BABYLON.Scene) {
-    this._createGround()
-    this._createSphere()
+    this._createGround();
+    this._createSphere();
   }
 
   _createGround(): void {
-    const { scene } = this
+    const { scene } = this;
 
-    const mesh = BABYLON.MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene)
-    new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.BOX, { mass: 0 }, scene)
+    const mesh = BABYLON.MeshBuilder.CreateGround(
+      "ground",
+      { width: 10, height: 10 },
+      scene
+    );
+    //   new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.BOX, { mass: 0 }, scene)
   }
 
   _createSphere(): void {
-    const mesh = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2, segments: 32 }, this.scene)
-    mesh.position.y = 4
+    const mesh = BABYLON.MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2, segments: 32 },
+      this.scene
+    );
+    mesh.position.y = 4;
 
-    new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.SPHERE, { mass: 1, restitution: 0.75 }, this.scene)
+    //   new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.SPHERE, { mass: 1, restitution: 0.75 }, this.scene)
   }
 }
--- src/playground/main-scene.ts
+++ src/playground/main-scene.ts
@@ -1,27 +1,43 @@
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
 // import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-import '@babylonjs/loaders'
+import "@babylonjs/loaders";
 
-import { Ground } from './ground'
+import { Ground } from "./ground";
 
 export default class MainScene {
-  private camera: BABYLON.ArcRotateCamera
+  private camera: BABYLON.ArcRotateCamera;
 
-  constructor(private scene: BABYLON.Scene, private canvas: HTMLCanvasElement, private engine: BABYLON.Engine) {
-    this._setCamera(scene)
-    this._setLight(scene)
-    this.loadComponents()
+  constructor(
+    private scene: BABYLON.Scene,
+    private canvas: HTMLCanvasElement,
+    private engine: BABYLON.Engine
+  ) {
+    this._setCamera(scene);
+    this._configureCamera();
+    this._setLight(scene);
+    this.loadComponents();
   }
 
   _setCamera(scene: BABYLON.Scene): void {
-    this.camera = new BABYLON.ArcRotateCamera('camera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(80), 20, BABYLON.Vector3.Zero(), scene)
-    this.camera.attachControl(this.canvas, true)
-    this.camera.setTarget(BABYLON.Vector3.Zero())
+    this.camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      BABYLON.Tools.ToRadians(90),
+      BABYLON.Tools.ToRadians(80),
+      20,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+    this.camera.attachControl(this.canvas, true);
+    this.camera.setTarget(BABYLON.Vector3.Zero());
   }
 
   _setLight(scene: BABYLON.Scene): void {
-    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene)
-    light.intensity = 0.7
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+    light.intensity = 0.7;
   }
 
   // _setPipeLine(): void {
@@ -30,6 +46,48 @@ export default class MainScene {
 
   loadComponents(): void {
     // Load your files in order
-    new Ground(this.scene)
+    new Ground(this.scene);
   }
+  //
+  _configureCamera() {
+    if (!this.scene.activeCamera) {
+      // Inline scene.createDefaultCamera to reduce file size
+      // Dispose existing camera in replace mode.
+      if (this.scene.activeCamera) {
+        (this.scene.activeCamera as BABYLON.ArcRotateCamera).dispose();
+        this.scene.activeCamera = null;
+      }
+      // Camera
+      if (!this.scene.activeCamera) {
+        const worldExtends = this.scene.getWorldExtends();
+        const worldSize = worldExtends.max.subtract(worldExtends.min);
+        const worldCenter = worldExtends.min.add(worldSize.scale(0.5));
+
+        let radius = worldSize.length() * 1.5;
+        // empty scene scenario!
+        if (!isFinite(radius)) {
+          radius = 1;
+          worldCenter.copyFromFloats(0, 0, 0);
+        }
+
+        const arcRotateCamera = new BABYLON.ArcRotateCamera(
+          "default camera",
+          -(Math.PI / 2),
+          Math.PI / 2,
+          radius,
+          worldCenter,
+          this.scene
+        );
+        arcRotateCamera.lowerRadiusLimit = radius * 0.01;
+        arcRotateCamera.wheelPrecision = 100 / radius;
+        const camera = arcRotateCamera;
+
+        camera.minZ = radius * 0.01;
+        camera.maxZ = radius * 1000;
+        camera.speed = radius * 0.2;
+        this.scene.activeCamera = camera;
+      }
+    }
+  }
+  //
 }
