f9c8bed9d40878cc4bd6b2d7431088fae63ef2e0
eldinor <eldinor@gmail.com>
2024-10-15 17:03:25 +0300

    Import from /core

--- src/playground/ground.ts
+++ src/playground/ground.ts
@@ -1,23 +1,41 @@
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-import '@babylonjs/loaders'
+import {
+  MeshBuilder,
+  PhysicsAggregate,
+  PhysicsShapeType,
+  Scene,
+} from "@babylonjs/core";
+import "@babylonjs/loaders";
 
 export class Ground {
-  constructor(private scene: BABYLON.Scene) {
-    this._createGround()
-    this._createSphere()
+  constructor(private scene: Scene) {
+    this._createGround();
+    this._createSphere();
   }
 
   _createGround(): void {
-    const { scene } = this
+    const { scene } = this;
 
-    const mesh = BABYLON.MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene)
-    new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.BOX, { mass: 0 }, scene)
+    const mesh = MeshBuilder.CreateGround(
+      "ground",
+      { width: 10, height: 10 },
+      scene
+    );
+    new PhysicsAggregate(mesh, PhysicsShapeType.BOX, { mass: 0 }, scene);
   }
 
   _createSphere(): void {
-    const mesh = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2, segments: 32 }, this.scene)
-    mesh.position.y = 4
+    const mesh = MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2, segments: 32 },
+      this.scene
+    );
+    mesh.position.y = 4;
 
-    new BABYLON.PhysicsAggregate(mesh, BABYLON.PhysicsShapeType.SPHERE, { mass: 1, restitution: 0.75 }, this.scene)
+    new PhysicsAggregate(
+      mesh,
+      PhysicsShapeType.SPHERE,
+      { mass: 1, restitution: 0.75 },
+      this.scene
+    );
   }
 }
--- src/playground/main-scene.ts
+++ src/playground/main-scene.ts
@@ -1,35 +1,58 @@
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-// import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-import '@babylonjs/loaders'
+import {
+  ArcRotateCamera,
+  DefaultRenderingPipeline,
+  Engine,
+  HemisphericLight,
+  Scene,
+  Tools,
+  Vector3,
+} from "@babylonjs/core";
+import "@babylonjs/loaders";
 
-import { Ground } from './ground'
+import { Ground } from "./ground";
 
 export default class MainScene {
-  private camera: BABYLON.ArcRotateCamera
+  private camera: ArcRotateCamera;
 
-  constructor(private scene: BABYLON.Scene, private canvas: HTMLCanvasElement, private engine: BABYLON.Engine) {
-    this._setCamera(scene)
-    this._setLight(scene)
-    this.loadComponents()
+  constructor(
+    private scene: Scene,
+    private canvas: HTMLCanvasElement,
+    private engine: Engine
+  ) {
+    this._setCamera(scene);
+    this._setLight(scene);
+    this.loadComponents();
   }
 
-  _setCamera(scene: BABYLON.Scene): void {
-    this.camera = new BABYLON.ArcRotateCamera('camera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(80), 20, BABYLON.Vector3.Zero(), scene)
-    this.camera.attachControl(this.canvas, true)
-    this.camera.setTarget(BABYLON.Vector3.Zero())
+  _setCamera(scene: Scene): void {
+    this.camera = new ArcRotateCamera(
+      "camera",
+      Tools.ToRadians(90),
+      Tools.ToRadians(80),
+      20,
+      Vector3.Zero(),
+      scene
+    );
+    this.camera.attachControl(this.canvas, true);
+    this.camera.setTarget(Vector3.Zero());
   }
 
-  _setLight(scene: BABYLON.Scene): void {
-    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene)
-    light.intensity = 0.7
+  _setLight(scene: Scene): void {
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
   }
 
-  // _setPipeLine(): void {
-  //   const pipeline = new BABYLON.DefaultRenderingPipeline('default-pipeline', false, this.scene, [this.scene.activeCamera!])
-  // }
+  _setPipeLine(): void {
+    const pipeline = new DefaultRenderingPipeline(
+      "default-pipeline",
+      false,
+      this.scene,
+      [this.scene.activeCamera!]
+    );
+  }
 
   loadComponents(): void {
     // Load your files in order
-    new Ground(this.scene)
+    new Ground(this.scene);
   }
 }
